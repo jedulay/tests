1b94a363fbdf9f71c471ffc3458493591979dde8
jedulay <dulayjenine@gmail.com>
2025-01-16 03:50:29 +0800

    comment section added

---
+++ home/js/comments.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#comments',
+    data: {
+      guestName: '',
+      guestComment: '',
+      comments: []
+    },
+    methods: {
+      addComment() {
+        const newComment = {
+          id: this.comments.length + 1,
+          name: this.guestName,
+          message: this.guestComment
+        };
+        this.comments.push(newComment);
+        this.guestName = '';
+        this.guestComment = '';
+      }
+    }
+  });
