664867a5fa94c83afab42e8cf42683d05cbc6a72
jedulay <dulayjenine@gmail.com>
2025-01-16 02:14:04 +0800

    added js and updated css and html

---
+++ home/js/guestbook.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#guestbook',
+    data: {
+      guestName: '',
+      guestMessage: ''
+    },
+    methods: {
+      addEntry() {
+        console.log('Name:', this.guestName);
+        console.log('Message:', this.guestMessage);
+        alert('Thank you for your message!');
+      }
+    }
+  });
