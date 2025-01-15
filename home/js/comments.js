a08f8cf43bed43a749f5b2fb84667bfe9644455f
jedulay <dulayjenine@gmail.com>
2025-01-16 04:10:22 +0800

    a

--- home/js/comments.js
+++ home/js/comments.js
@@ -1,20 +1,25 @@
-new Vue({
-  el: '#comments',
-  data: {
-    guestName: '',
-    guestComment: '',
-    comments: []
-  },
-  methods: {
-    addComment() {
-      const newComment = {
-        id: this.comments.length + 1,
-        name: this.guestName,
-        message: this.guestComment
+
+  const app = Vue.createApp({
+    data() {
+      return {
+        guestName: '',
+        guestComment: '',
+        comments: []
       };
-      this.comments.push(newComment);
-      this.guestName = '';
-      this.guestComment = '';
+    },
+    methods: {
+      addComment() {
+        const newComment = {
+          id: this.comments.length + 1,
+          name: this.guestName,
+          message: this.guestComment
+        };
+        this.comments.push(newComment);
+        this.guestName = '';
+        this.guestComment = '';
+      }
     }
-  }
-});
+  });
+
+  app.mount('#comments');
+
